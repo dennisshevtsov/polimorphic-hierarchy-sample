777fe6af73d4242f506b43691c6a56c46fdfc8b6
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2023-11-05 16:48:19 +0300

    add questions

---
+++ src/PolimorphicJsonSample/TextQuestion.cs
@@ -0,0 +1,8 @@
+﻿// Copyright (c) Dennis Shevtsov. All rights reserved.
+// Licensed under the MIT License.
+// See LICENSE in the project root for license information.
+
+namespace PolimorphicJsonSample;
+
+public sealed record class TextQuestion(string Text)
+  : QuestionBase(Text, QuestionType.Text);
