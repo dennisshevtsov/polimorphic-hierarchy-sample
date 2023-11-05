9efa2cd999e082371b18a0dcc16afed9e15f4734
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2023-11-05 16:40:10 +0300

    add question base

---
+++ src/PolimorphicJsonSample/QuestionBase.cs
@@ -0,0 +1,7 @@
+﻿// Copyright (c) Dennis Shevtsov. All rights reserved.
+// Licensed under the MIT License.
+// See LICENSE in the project root for license information.
+
+namespace PolimorphicJsonSample;
+
+public abstract record class QuestionBase(string Text, QuestionType Type);
