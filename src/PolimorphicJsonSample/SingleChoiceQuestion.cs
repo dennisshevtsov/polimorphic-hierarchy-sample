91e6f34031bed0148adec84f4813c73b3f75febe
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2023-11-05 16:53:55 +0300

    add attributes

--- src/PolimorphicJsonSample/SingleChoiceQuestion.cs
+++ src/PolimorphicJsonSample/SingleChoiceQuestion.cs
@@ -2,7 +2,14 @@
 // Licensed under the MIT License.
 // See LICENSE in the project root for license information.
 
+using System.Text.Json.Serialization;
+
 namespace PolimorphicJsonSample;
 
+[JsonPolymorphic(TypeDiscriminatorPropertyName = "questionType")]
+[JsonDerivedType(typeof(TextQuestion)          , (int)QuestionType.Text          )]
+[JsonDerivedType(typeof(YesNoQuestion)         , (int)QuestionType.YesNo         )]
+[JsonDerivedType(typeof(MultipleChoiceQuestion), (int)QuestionType.MultipleChoice)]
+[JsonDerivedType(typeof(SingleChoiceQuestion)  , (int)QuestionType.SingleChoice  )]
 public sealed record class SingleChoiceQuestion(string Text, string[] Choices, string? Answer)
   : QuestionBase(Text, QuestionType.Text);
