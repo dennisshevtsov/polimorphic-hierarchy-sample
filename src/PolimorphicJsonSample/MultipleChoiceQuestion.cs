69b3b35161a53c5a7fefc5f145a311631ecac4dc
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2023-11-06 19:03:37 +0300

    define own equals method to compare multiple choice question records

--- src/PolimorphicJsonSample/MultipleChoiceQuestion.cs
+++ src/PolimorphicJsonSample/MultipleChoiceQuestion.cs
@@ -10,4 +10,37 @@ public sealed record class MultipleChoiceQuestion(
   string Text,
   [property: JsonPropertyOrder(2)] string[] Choices,
   [property: JsonPropertyOrder(3)] string[] Answers)
-  : QuestionBase(Text, QuestionType.MultipleChoice);
+  : QuestionBase(Text, QuestionType.MultipleChoice)
+{
+#pragma warning disable CS8851 // Record defines 'Equals' but not 'GetHashCode'.
+  public bool Equals(MultipleChoiceQuestion? other)
+#pragma warning restore CS8851 // Record defines 'Equals' but not 'GetHashCode'.
+  {
+    if (other is null)
+    {
+      return false;
+    }
+
+    if (object.ReferenceEquals(this, other))
+    {
+      return true;
+    }
+
+    if (Text != other.Text)
+    {
+      return false;
+    }
+
+    if (!QuestionBase.Equals(Choices, other.Choices))
+    {
+      return false;
+    }
+
+    if (!QuestionBase.Equals(Answers, other.Answers))
+    {
+      return false;
+    }
+
+    return true;
+  }
+}
