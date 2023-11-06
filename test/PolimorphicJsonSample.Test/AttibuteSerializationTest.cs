4dad34fd0753e197383e38096ac73594d93fb9e4
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2023-11-06 19:04:18 +0300

    test deserialize

--- test/PolimorphicJsonSample.Test/AttibuteSerializationTest.cs
+++ test/PolimorphicJsonSample.Test/AttibuteSerializationTest.cs
@@ -100,4 +100,101 @@ public sealed class AttibuteSerializationTest
 
     Assert.AreEqual(Regex.Replace(expected, "\\s+", ""), actual);
   }
+
+  [TestMethod]
+  public void Deserialize_Json_ArrayOfDerivedObjectsDeserialized()
+  {
+    // Arrange
+    string json = @"
+    [
+      {
+        ""type""  : 0,
+        ""text""  : ""text_question:text"",
+        ""answer"": ""text_question:answer""
+      },
+      {
+        ""type""  : 1,
+        ""text""  : ""yes_no_question:text"",
+        ""answer"": 1
+      },
+      {
+        ""type""   : 2,
+        ""text""   : ""multiple_choice_question:text"",
+        ""choices"": [
+          ""multiple_choice_question:choice:0"",
+          ""multiple_choice_question:choice:1"",
+          ""multiple_choice_question:choice:2""
+        ],
+        ""answers"": [
+          ""multiple_choice_question:choice:0"",
+          ""multiple_choice_question:choice:2""
+        ]
+      },
+      {
+        ""type""   : 3,
+        ""text""   : ""single_choice_question:text"",
+        ""choices"": [
+          ""single_choice_question:choice:0"",
+          ""single_choice_question:choice:1"",
+          ""single_choice_question:choice:2""
+        ],
+        ""answer"" : ""single_choice_question:choice:1""
+      }
+    ]";
+
+    // Act
+    QuestionBase[]? actual = JsonSerializer.Deserialize<QuestionBase[]>(json, new JsonSerializerOptions
+    {
+       PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+    });
+
+    // Assert
+    QuestionBase[] expected =
+    {
+      new TextQuestion
+      (
+        Text  : "text_question:text",
+        Answer: "text_question:answer"
+      ),
+      new YesNoQuestion
+      (
+        Text  : "yes_no_question:text",
+        Answer: YesNo.Yes
+      ),
+      new MultipleChoiceQuestion
+      (
+        Text   : "multiple_choice_question:text",
+        Choices: new[]
+        {
+          "multiple_choice_question:choice:0",
+          "multiple_choice_question:choice:1",
+          "multiple_choice_question:choice:2",
+        },
+        Answers : new[]
+        {
+          "multiple_choice_question:choice:0",
+          "multiple_choice_question:choice:2",
+        }
+      ),
+      new SingleChoiceQuestion
+      (
+        Text   : "single_choice_question:text",
+        Choices: new[]
+        {
+          "single_choice_question:choice:0",
+          "single_choice_question:choice:1",
+          "single_choice_question:choice:2",
+        },
+        Answer : "single_choice_question:choice:1"
+      ),
+    };
+
+    Assert.IsNotNull(actual);
+    Assert.AreEqual(expected.Length, actual.Length);
+
+    for (int i = 0; i < expected.Length; i++)
+    {
+      Assert.AreEqual(expected[i], actual[i]);
+    }
+  }
 }
