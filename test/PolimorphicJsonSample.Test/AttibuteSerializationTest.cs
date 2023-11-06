77d21c94925116df4834bcf4af693bef9de808b4
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2023-11-06 19:10:15 +0300

    refactor test

--- test/PolimorphicJsonSample.Test/AttibuteSerializationTest.cs
+++ test/PolimorphicJsonSample.Test/AttibuteSerializationTest.cs
@@ -14,45 +14,7 @@ public sealed class AttibuteSerializationTest
   public void Serialize_ArrayOfBases_ArrayOfDerivedObjectsSerialized()
   {
     // Arrange
-    QuestionBase[] questions =
-    {
-      new TextQuestion
-      (
-        Text  : "text_question:text",
-        Answer: "text_question:answer"
-      ),
-      new YesNoQuestion
-      (
-        Text  : "yes_no_question:text",
-        Answer: YesNo.Yes
-      ),
-      new MultipleChoiceQuestion
-      (
-        Text   : "multiple_choice_question:text",
-        Choices: new[]
-        {
-          "multiple_choice_question:choice:0",
-          "multiple_choice_question:choice:1",
-          "multiple_choice_question:choice:2",
-        },
-        Answers : new[]
-        {
-          "multiple_choice_question:choice:0",
-          "multiple_choice_question:choice:2",
-        }
-      ),
-      new SingleChoiceQuestion
-      (
-        Text   : "single_choice_question:text",
-        Choices: new[]
-        {
-          "single_choice_question:choice:0",
-          "single_choice_question:choice:1",
-          "single_choice_question:choice:2",
-        },
-        Answer : "single_choice_question:choice:1"
-      ),
-    };
+    QuestionBase[] questions = AttibuteSerializationTest.CreateTestQuestions();
 
     // Act
     string actual = JsonSerializer.Serialize(questions, new JsonSerializerOptions
@@ -61,86 +23,15 @@ public sealed class AttibuteSerializationTest
     });
 
     // Assert
-    string expected = @"
-    [
-      {
-        ""type""  : 0,
-        ""text""  : ""text_question:text"",
-        ""answer"": ""text_question:answer""
-      },
-      {
-        ""type""  : 1,
-        ""text""  : ""yes_no_question:text"",
-        ""answer"": 1
-      },
-      {
-        ""type""   : 2,
-        ""text""   : ""multiple_choice_question:text"",
-        ""choices"": [
-          ""multiple_choice_question:choice:0"",
-          ""multiple_choice_question:choice:1"",
-          ""multiple_choice_question:choice:2""
-        ],
-        ""answers"": [
-          ""multiple_choice_question:choice:0"",
-          ""multiple_choice_question:choice:2""
-        ]
-      },
-      {
-        ""type""   : 3,
-        ""text""   : ""single_choice_question:text"",
-        ""choices"": [
-          ""single_choice_question:choice:0"",
-          ""single_choice_question:choice:1"",
-          ""single_choice_question:choice:2""
-        ],
-        ""answer"" : ""single_choice_question:choice:1""
-      }
-    ]";
-
-    Assert.AreEqual(Regex.Replace(expected, "\\s+", ""), actual);
+    string expected = Regex.Replace(AttibuteSerializationTest.CreateTestJson(), "\\s+", "");
+    Assert.AreEqual(expected, actual);
   }
 
   [TestMethod]
   public void Deserialize_Json_ArrayOfDerivedObjectsDeserialized()
   {
     // Arrange
-    string json = @"
-    [
-      {
-        ""type""  : 0,
-        ""text""  : ""text_question:text"",
-        ""answer"": ""text_question:answer""
-      },
-      {
-        ""type""  : 1,
-        ""text""  : ""yes_no_question:text"",
-        ""answer"": 1
-      },
-      {
-        ""type""   : 2,
-        ""text""   : ""multiple_choice_question:text"",
-        ""choices"": [
-          ""multiple_choice_question:choice:0"",
-          ""multiple_choice_question:choice:1"",
-          ""multiple_choice_question:choice:2""
-        ],
-        ""answers"": [
-          ""multiple_choice_question:choice:0"",
-          ""multiple_choice_question:choice:2""
-        ]
-      },
-      {
-        ""type""   : 3,
-        ""text""   : ""single_choice_question:text"",
-        ""choices"": [
-          ""single_choice_question:choice:0"",
-          ""single_choice_question:choice:1"",
-          ""single_choice_question:choice:2""
-        ],
-        ""answer"" : ""single_choice_question:choice:1""
-      }
-    ]";
+    string json = AttibuteSerializationTest.CreateTestJson();
 
     // Act
     QuestionBase[]? actual = JsonSerializer.Deserialize<QuestionBase[]>(json, new JsonSerializerOptions
@@ -149,45 +40,7 @@ public sealed class AttibuteSerializationTest
     });
 
     // Assert
-    QuestionBase[] expected =
-    {
-      new TextQuestion
-      (
-        Text  : "text_question:text",
-        Answer: "text_question:answer"
-      ),
-      new YesNoQuestion
-      (
-        Text  : "yes_no_question:text",
-        Answer: YesNo.Yes
-      ),
-      new MultipleChoiceQuestion
-      (
-        Text   : "multiple_choice_question:text",
-        Choices: new[]
-        {
-          "multiple_choice_question:choice:0",
-          "multiple_choice_question:choice:1",
-          "multiple_choice_question:choice:2",
-        },
-        Answers : new[]
-        {
-          "multiple_choice_question:choice:0",
-          "multiple_choice_question:choice:2",
-        }
-      ),
-      new SingleChoiceQuestion
-      (
-        Text   : "single_choice_question:text",
-        Choices: new[]
-        {
-          "single_choice_question:choice:0",
-          "single_choice_question:choice:1",
-          "single_choice_question:choice:2",
-        },
-        Answer : "single_choice_question:choice:1"
-      ),
-    };
+    QuestionBase[] expected = AttibuteSerializationTest.CreateTestQuestions();
 
     Assert.IsNotNull(actual);
     Assert.AreEqual(expected.Length, actual.Length);
@@ -197,4 +50,81 @@ public sealed class AttibuteSerializationTest
       Assert.AreEqual(expected[i], actual[i]);
     }
   }
+
+  private static string CreateTestJson() => @"
+  [
+    {
+      ""type""  : 0,
+      ""text""  : ""text_question:text"",
+      ""answer"": ""text_question:answer""
+    },
+    {
+      ""type""  : 1,
+      ""text""  : ""yes_no_question:text"",
+      ""answer"": 1
+    },
+    {
+      ""type""   : 2,
+      ""text""   : ""multiple_choice_question:text"",
+      ""choices"": [
+        ""multiple_choice_question:choice:0"",
+        ""multiple_choice_question:choice:1"",
+        ""multiple_choice_question:choice:2""
+      ],
+      ""answers"": [
+        ""multiple_choice_question:choice:0"",
+        ""multiple_choice_question:choice:2""
+      ]
+    },
+    {
+      ""type""   : 3,
+      ""text""   : ""single_choice_question:text"",
+      ""choices"": [
+        ""single_choice_question:choice:0"",
+        ""single_choice_question:choice:1"",
+        ""single_choice_question:choice:2""
+      ],
+      ""answer"" : ""single_choice_question:choice:1""
+    }
+  ]";
+
+  private static QuestionBase[] CreateTestQuestions() => new QuestionBase[]
+  {
+    new TextQuestion
+    (
+      Text  : "text_question:text",
+      Answer: "text_question:answer"
+    ),
+    new YesNoQuestion
+    (
+      Text  : "yes_no_question:text",
+      Answer: YesNo.Yes
+    ),
+    new MultipleChoiceQuestion
+    (
+      Text   : "multiple_choice_question:text",
+      Choices: new[]
+      {
+        "multiple_choice_question:choice:0",
+        "multiple_choice_question:choice:1",
+        "multiple_choice_question:choice:2",
+      },
+      Answers : new[]
+      {
+        "multiple_choice_question:choice:0",
+        "multiple_choice_question:choice:2",
+      }
+    ),
+    new SingleChoiceQuestion
+    (
+      Text   : "single_choice_question:text",
+      Choices: new[]
+      {
+        "single_choice_question:choice:0",
+        "single_choice_question:choice:1",
+        "single_choice_question:choice:2",
+      },
+      Answer : "single_choice_question:choice:1"
+    ),
+  };
 }
