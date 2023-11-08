65a34e14d06904d889e1276e88f77bfbdcae2ce5
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2023-11-08 22:06:00 +0300

    add test data

--- test/PolimorphicJsonSample.ContractModel.Test/ContractModelSerializationTest.cs
+++ test/PolimorphicJsonSample.ContractModel.Test/ContractModelSerializationTest.cs
@@ -2,9 +2,88 @@
 // Licensed under the MIT License.
 // See LICENSE in the project root for license information.
 
+using System.Text.Json;
+using System.Text.RegularExpressions;
+
 namespace PolimorphicJsonSample.ContractModel.Test;
 
 [TestClass]
 public sealed class ContractModelSerializationTest
 {
+  private static string CreateTestJson() => @"
+  [
+    {
+      ""type""  : 0,
+      ""text""  : ""text_question:text"",
+      ""answer"": ""text_question:answer""
+    },
+    {
+      ""type""  : 1,
+      ""text""  : ""yes_no_question:text"",
+      ""answer"": 1
+    },
+    {
+      ""type""   : 2,
+      ""text""   : ""multiple_choice_question:text"",
+      ""choices"": [
+        ""multiple_choice_question:choice:0"",
+        ""multiple_choice_question:choice:1"",
+        ""multiple_choice_question:choice:2""
+      ],
+      ""answers"": [
+        ""multiple_choice_question:choice:0"",
+        ""multiple_choice_question:choice:2""
+      ]
+    },
+    {
+      ""type""   : 3,
+      ""text""   : ""single_choice_question:text"",
+      ""choices"": [
+        ""single_choice_question:choice:0"",
+        ""single_choice_question:choice:1"",
+        ""single_choice_question:choice:2""
+      ],
+      ""answer"" : ""single_choice_question:choice:1""
+    }
+  ]";
+
+  private static QuestionBase[] CreateTestQuestions() => new QuestionBase[]
+  {
+    new TextQuestion
+    (
+      Text  : "text_question:text",
+      Answer: "text_question:answer"
+    ),
+    new YesNoQuestion
+    (
+      Text  : "yes_no_question:text",
+      Answer: YesNo.Yes
+    ),
+    new MultipleChoiceQuestion
+    (
+      Text   : "multiple_choice_question:text",
+      Choices: new[]
+      {
+        "multiple_choice_question:choice:0",
+        "multiple_choice_question:choice:1",
+        "multiple_choice_question:choice:2",
+      },
+      Answers : new[]
+      {
+        "multiple_choice_question:choice:0",
+        "multiple_choice_question:choice:2",
+      }
+    ),
+    new SingleChoiceQuestion
+    (
+      Text   : "single_choice_question:text",
+      Choices: new[]
+      {
+        "single_choice_question:choice:0",
+        "single_choice_question:choice:1",
+        "single_choice_question:choice:2",
+      },
+      Answer : "single_choice_question:choice:1"
+    ),
+  };
 }
