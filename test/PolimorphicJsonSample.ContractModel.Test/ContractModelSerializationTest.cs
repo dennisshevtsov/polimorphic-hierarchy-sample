4c25dbfdc6fa6e4b9c47bcd6c82347eff1f50446
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2023-11-08 22:03:18 +0300

    add contract model serialization test

---
+++ test/PolimorphicJsonSample.ContractModel.Test/ContractModelSerializationTest.cs
@@ -0,0 +1,10 @@
+﻿// Copyright (c) Dennis Shevtsov. All rights reserved.
+// Licensed under the MIT License.
+// See LICENSE in the project root for license information.
+
+namespace PolimorphicJsonSample.ContractModel.Test;
+
+[TestClass]
+public sealed class ContractModelSerializationTest
+{
+}
